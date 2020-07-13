9c68e242de4c25213b5c219638972b4bc6d8ac66
Mason <Mudland2721@gamil.com>
2020-07-13 17:58:00 -0600

    added sql scheamas connect.js and started orms

---
+++ config/orm.js
@@ -0,0 +1,42 @@
+// need three function
+//selectAll()
+//insertOne()
+//updateOne()
+
+// Import MYSQL connection.
+var connection = require("../config/connection.js");
+const { query } = require("express");
+
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+  let arr = [];
+
+  for (let i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
+
+// Helper function for SQL syntax.
+function objToSql(ob) {
+  let arr = [];
+
+  for (let key in ob) {
+    if (Object.hasOwnProperty.call(ob, key)) {
+      arr.push(key + "=" + ob[key]);
+    }
+  }
+
+  return arr.toString();
+}
+
+//select all function
+const orm = {
+  selectAll: function (tableInput, burger) {
+    const queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, res) {
+      if (err) throw err;
+      burger(res);
+    });
+  },
+};
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES("Royal With Cheese");
+INSERT INTO burgers
+    (burger_name)
+VALUES("Sin City");
+INSERT INTO burgers
+    (burger_name)
+VALUES("Campfire");
