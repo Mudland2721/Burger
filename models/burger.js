ee267896d9d8e0be0dafd35a989a907889eabe0a
Mason <Mudland2721@gamil.com>
2020-07-13 20:21:36 -0600

    draft of burger.js

--- models/burger.js
+++ models/burger.js
@@ -6,12 +6,18 @@ const burger = {
       burger(res);
     });
   },
+  // The variables cols and vals are arrays.
   create: function (cols, vals, burger) {
     orm.create("burgers", cols, vals, function (res) {
       burger(res);
     });
   },
-  // update: function(objColVal, condition, burger) {
-  //     orm.update("burgers", objColVal)
-  // }
+  update: function (objColVal, condition, burger) {
+    orm.update("burgers", objColVal, condition, function (res) {
+      burger(res);
+    });
+  },
 };
+
+// export to controller
+module.exports(burger);
