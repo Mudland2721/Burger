67bf29e3604f82ddf16b8350d962de71a138ca2d
Mason <Mudland2721@gamil.com>
2020-07-13 20:19:40 -0600

    started burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,17 @@
 const orm = require("../config/orm.js");
 
 const burger = {
-    a
-}
+  all: function (burger) {
+    orm.all("burgers", function (res) {
+      burger(res);
+    });
+  },
+  create: function (cols, vals, burger) {
+    orm.create("burgers", cols, vals, function (res) {
+      burger(res);
+    });
+  },
+  // update: function(objColVal, condition, burger) {
+  //     orm.update("burgers", objColVal)
+  // }
+};
