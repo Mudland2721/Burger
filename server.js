fb7b01ad0fa46f93a58362bcf097598143a792f2
Mason <Mudland2721@gamil.com>
2020-07-20 07:03:19 -0600

    added server and main handlebars

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express = require("express");
+const app = express();
+const exphbs = require("express-handlebars");
+
+const port = process.env.PORT || 8080;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import burger routes
+const routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(port);
+console.log("localhost: 8080");
