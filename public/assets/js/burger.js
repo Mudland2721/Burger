7329f4c0ce236fd23e08fb89d27bcb0e94d17edb
Mason <Mudland2721@gamil.com>
2020-07-20 20:35:06 -0600

    base for frontend js

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,3 +1,28 @@
 $(function () {
-    $("")
-})
+  $(".not-devoured").on("click", function (event) {
+    let id = $(this).data("id");
+    let eaten = $(this).data("devoured");
+
+    let condition = {
+      notEaten: eaten,
+    };
+    $.ajax("/api/burger/" + id, {
+      type: "PUT",
+      data: condition,
+    }).then(function () {
+      console.log(`changed burger to`, eaten);
+      location.reload();
+    });
+  });
+});
+
+$(".create-from").on("submit", function (event) {
+  // Make sure to preventDefault on a submit event.
+  event.preventDefault();
+
+    let newBurg = {
+        name: $("#burg").val().trim(),
+        notEaten: $("[burger_name]:")
+    }
+
+});
