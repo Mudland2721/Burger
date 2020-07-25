a984cec4b61216aa0e66d836367fd396d21c691c
Mason <Mudland2721@gamil.com>
2020-07-25 11:05:02 -0600

    added compression to server

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 const exphbs = require("express-handlebars");
 const routes = require("./controllers/burgerController.js");
 const port = process.env.PORT || 8080;
+const compression = require('compression')
+ 
+ 
+// compress all responses
+app.use(compression())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
